fc931242d3f3641bc31369e3a41ed0a09ce9b4ea
Raghav <Raghav@192.168.0.12>
2018-08-24 10:37:07 +0530

    Started Leetcode

---
+++ Hello/src/letsLeetCode/ArrayDynamic.java
@@ -0,0 +1,13 @@
+package letsLeetCode;
+
+public class ArrayDynamic {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		List<Integer> list1= new ArrayList<Integer>();
+		
+	}
+
+}
