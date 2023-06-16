fc2c5df3a8e35de5653fc21fdfdee2fe75586d30
raghavBakshee <raghav_bakshee@yahoo.in>
2023-06-16 16:19:19 +0530

    Java problems

---
+++ Hello/src/letsLeetCode/FindPairClosestTo0.java
@@ -0,0 +1,26 @@
+package letsLeetCode;
+
+public class FindPairClosestTo0 {
+    public static void  findPairSum(int arr[]){
+        int minsum = arr[0] + arr[1];
+        int temp = 0;
+        int index1=0;
+        int index2 = 0;
+        
+        for(int i = 0; i < arr.length; i++){
+            for(int j = i + 1; j < arr.length; j++){
+                temp = arr[i] + arr[j];
+                if(Math.abs(temp) < Math.abs(minsum)){
+                    index1 = i;
+                    index2 = j;
+                    minsum = Math.abs(temp);
+                }
+            }
+        }
+       System.out.print(arr[index1] + "   "+ arr[index2]);
+    }
+    public static void main(String[] args) {
+        findPairSum(new int[] {1,3,-5,7,8,20,-40,6});
+        //System.out.println("Hello, World!");
+    }
+}
